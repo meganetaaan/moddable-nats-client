b331eff750a04ba9f92bfb22470cbed832a72e9b
Shinya ISHIKAWA <ishikawa.s.1027@gmail.com>
2024-09-01 14:40:47 +0900

    WIP: subscription

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import { NATS } from "nats";
 import Timer from "timer";
 
 const client = new NATS("127.0.0.1", 4222, "user", "hoge", false);
+client.subscribe("hoge", (msg) => {
+	trace(`got hoge: ${JSON.stringify(msg)}\n`)
+})
 client.onConnect = () => {
 	trace("connected\n");
 	let count = 0
